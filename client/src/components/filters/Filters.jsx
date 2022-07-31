6eca9d0a8d363f930be527666dcf8c692f1f301a
unknown <martinfigueroa2103@hotmail.com>
2022-07-31 20:40:54 -0300

    Estilos1  commit

--- client/src/components/filters/Filters.jsx
+++ client/src/components/filters/Filters.jsx
@@ -18,17 +18,19 @@ function onChageExist(e){
 
 
     return (
-        <div >
-        <h2>Filtros</h2>
+        <div className="total" >
+           
+
+        <div className="filtros">    
+     
+
         <div className="form__group" >
-        
         <select className="form__field w-100" onChange={onChageExist}>
             <option value="" disabled selected>Filtro creados</option>
             <option value="all">Todos</option>
             <option value="exist">De API</option>
             <option value="created">De database</option>
         </select>
-
 
         <select className="form__field w-100" onChange={(e) => handleFilterTemp(e)}>
             <option value="" disabled selected>Filtro por temperamento</option>
@@ -37,22 +39,24 @@ function onChageExist(e){
             <option key={e} value={e}>{e}</option>
             ))}
             </select>
-
-        <h2>Orden por:</h2> 
+        </div>
+          
+        <div className="orden">
+        
         <select className="form__field w-100" onChange={(e) => handleSortWeight(e)}>
             <option value=""disabled selected >Orden por peso</option>
             <option value="weightasc">Mas pesado</option>
             <option value="weightdesc">Mas liviano</option>
         </select>
 
-        <label>Razas</label>
         <select className="form__field w-100" onChange={(e) => handleSort(e)}>
             <option value=""disabled selected >Orden alfabetico</option>
             <option value="asc">A-Z</option>
             <option value="desc">Z-A</option>
         </select>
-
         </div>
+        </div>
+
         <button onClick={(e)=>handleReset(e)}>Recargar</button>
         
         </div>
--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -6,15 +6,15 @@ import "./NavBar.css"
 export default function Navbar({setCurrentPage}) {
   return (
     <div className='navBar'>
-      <div className='titulo'>
-        <h1>Todos los perros del mundo</h1>
+      <div className='titulo' >
+        <h1>Perros del mundo</h1>
       </div>
       <div className="search">
         <SearchBar 
         setCurrent Page={setCurrentPage}
       />
       </div>
-      <div >
+      <div className='link'>
           <Link to="/post">
           <p>Creá tu propio perro</p>
           </Link>
